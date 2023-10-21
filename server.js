b85498704fcf02907664401312f2aa1d66807aec
roshanjha31 <jharoshan618@gmail.com>
2023-10-21 23:12:39 +0530

    cors added in backend

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
-
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(cookieParser())
 
 const router = require('./routes/portfolioRoute')
 
