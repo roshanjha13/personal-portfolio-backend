00208a8efe4c233cfcba23f7b608f8dab1e8ef4b
roshanjha31 <jharoshan618@gmail.com>
2023-10-23 17:15:24 +0530

    About section update is completed

--- controllers/getAllPortfolioController.js
+++ controllers/getAllPortfolioController.js
@@ -47,3 +47,20 @@ exports.getPortFolio = async (req, res) => {
     res.status(500).send(error);
   }
 };
+//update intro
+ exports.updateAbout = async (req, res) => {
+  try {
+    const about = await About.findOneAndUpdate(
+      { _id: req.body._id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).send({
+      data: about,
+      success: true,
+      message: "About updated successfully",
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getPortFolio, updateIntro } = require("../controllers/getAllPortfolioController");
+const { getPortFolio, updateIntro, updateAbout } = require("../controllers/getAllPortfolioController");
 
 const router = express.Router();
 
 router.route("/get-portfolio-data").get(getPortFolio);
 
 router.route('/update-intro').put(updateIntro)
+router.route('/update-about').put(updateAbout)
 
 module.exports = router;
